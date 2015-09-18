17dcc39141fb8b69db27201ef1b452bc02f954ee
Andrew Ilyas <ailyas@mit.edu>
2015-09-18 13:48:11 -0400

    CSVified

--- formgenerator.py
+++ formgenerator.py
@@ -1,16 +1,22 @@
 import pdfkit
 import dropbox
+import pandas as pd
 
+
+allsubmissions = pd.read_csv('topten.csv')
 fx = open('topten.txt')
-topten = []
-for thisline in fx.readlines():
-    line = thisline.split(" // ")
-    title = line[0]
-    members = line[1]
-    description = line[2]
-    tablenum = line[3]
-    imageurl = "Hack MIT - " + tablenum + "-" + title + ".jpg"
-    topten += [{"TITLE": title, "MEMBERS": members, "DESCRIPTION": description,  "IMAGEURL": imageurl}]
+topten = [x[:-1] for x in fx.readlines()]
+print topten
+toptensubs = allsubmissions[(allsubmissions['#'].isin(topten))]
+# topten = []
+# for thisline in fx.readlines():
+#     line = thisline.split(" // ")
+#     title = line[0]
+#     members = line[1]
+#     description = line[2]
+#     tablenum = line[3]
+#     imageurl = "Hack MIT - " + tablenum + "-" + title + ".jpg"
+#     topten += [{"TITLE": title, "MEMBERS": members, "DESCRIPTION": description,  "IMAGEURL": imageurl}]
 
 app_key = 'm5n5yem1mn8cizq'
 app_secret = 'v19hdvge5wujisq'
@@ -27,20 +33,20 @@ print 'linked account: ', client.account_info()
 f = open("newsheet.html", "w")
 f.write("")
 f.close()
-
-for top in topten:
+#
+for index, row in toptensubs.iterrows():
     f = open("judgessheet.html")
     theFile = f.read()
-    print top["IMAGEURL"]
-    faf, metadata = client.get_file_and_metadata('/File requests/HackMIT Photo Submissions/' + top["IMAGEURL"])
-    out = open(top["IMAGEURL"], 'wb')
+    faf, metadata = client.get_file_and_metadata('/File requests/HackMIT Photo Submissions/Hack MIT - ' + str(row["Table Number"]) + "-" + row["Project Title"] + ".jpg")
+
+    out = open(str(row["Table Number"]) + row["Project Title"] + ".jpg", 'wb')
     out.write(faf.read())
     out.close()
-    for x in top:
-        theFile = theFile.replace("$$" + x + "$$", top[x])
+    theFile = theFile.replace("$$TITLE$$", row["Project Title"])
+    theFile = theFile.replace("$$MEMBERS$$", row["List of team members"])
+    theFile = theFile.replace("$$DESCRIPTION$$", row["List of team members"])
+    theFile = theFile.replace("$$IMAGEURL$$", str(row["Table Number"]) + row["Project Title"] + ".jpg")
     f2 = open("newsheet.html", "a")
     f2.write(theFile)
     f2.close()
 pdfkit.from_file('newsheet.html', 'test.pdf')
-
-
